fa2e6ca8d72554be7837f8c8b12c587406028dad
guest <guest@axa-prj-02.doc.ic.ac.uk>
2013-10-13 13:43:46 +0100

    --Set up all models apart from tests and classes. Needs testing. --Should be visible in admin page.

--- djangoSRV/exercises/admin.py
+++ djangoSRV/exercises/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from exercises.models import Exercises
+from exercises.models import *
 
 admin.site.register(Exercises)
+admin.site.register(ModelSolutions)
+admin.site.register(StudentSubmissions)
+admin.site.register(Students)
+admin.site.register(Teachers)
--- djangoSRV/exercises/models.py
+++ djangoSRV/exercises/models.py
@@ -7,3 +7,27 @@ class Exercises(models.Model):
     title = models.CharField('Title', max_length=100)
     content = models.TextField('Content', blank=True)
     description = models.TextField('Description')
+
+
+class ModelSolutions(models.Model):
+    ex_id = models.ForeignKey('Exercises', primary_key=True)
+    content = models.TextField('Content')
+
+class StudentSubmissions(models.Model):
+    stu_id = models.ForeignKey('Students')
+    ex_id = models.ForeignKey('Exercises')
+    content = models.TextField('Content', blank=True)
+    submit_time = models.DateTimeField('Time of Submission',
+        auto_now_add=True)
+
+
+class Teachers(models.Model):
+    tch_id = models.AutoField('Teacher ID', primary_key=True)
+    name = models.CharField('Name', max_length=50)
+
+
+class Students(models.Model):
+    stu_id = models.AutoField('Teacher ID', primary_key=True)
+    name = models.CharField('Name', max_length=50)
+
+
