d7387092978f23eeddfa87dd667f1abbbe365630
Mihai Popescu <popescu.mihai.mp@gmail.com>
2013-10-14 15:02:54 +0300

    --Full database up, starting URL config

--- djangoSRV/exercises/admin.py
+++ djangoSRV/exercises/admin.py
@@ -1,8 +1,14 @@
+# This file registers all the models to the general django admin site.
+# It's only useful for monitoring and changing the database state in
+# a graphical context
+
 from django.contrib import admin
 from exercises.models import *
 
 admin.site.register(Exercise)
 admin.site.register(ModelSolution)
 admin.site.register(StudentSubmission)
+admin.site.register(Test)
+admin.site.register(Course)
 admin.site.register(Student)
 admin.site.register(Teacher)
--- djangoSRV/exercises/models.py
+++ djangoSRV/exercises/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# TODO: think about separating whether centralising it like this is optimal
 
 class Exercise(models.Model):
     ex_id = models.AutoField('Exercise ID', primary_key=True)
@@ -13,12 +14,24 @@ class ModelSolution(models.Model):
     ex_id = models.ForeignKey('Exercise', primary_key=True)
     content = models.TextField('Content')
 
+
 class StudentSubmission(models.Model):
     stu_id = models.ForeignKey('Student')
     ex_id = models.ForeignKey('Exercise')
     content = models.TextField('Content', blank=True)
     submit_time = models.DateTimeField('Time of Submission',
         auto_now_add=True)
+
+
+class Test(models.Model):
+    ex_id = models.ForeignKey('Exercise')
+    test_content = models.TextField('Test Content')
+
+
+class Course(models.Model):
+    c_id = models.AutoField('Course ID' , primary_key=True)
+    name = models.CharField('Course Name', max_length=50)
+    tch_id = models.ForeignKey('Teacher')
 
 
 class Teacher(models.Model):
