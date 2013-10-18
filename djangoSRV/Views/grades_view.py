25c02d88d42376eb808065d05b27b0500a8efb3b
Varun Verma <vv311@ic.ac.uk>
2013-10-18 02:53:36 +0100

    cleaned up the grades page and added more for loops for the data to be displayed well. now if db queries added. grades should work

--- djangoSRV/Views/grades_view.py
+++ djangoSRV/Views/grades_view.py
@@ -4,14 +4,29 @@ from django.template import Context
 
 def get_grades_view(request):
 	template = get_template("grades.html")
-	grades = __get_student_grades("vv311")
+	student_id = "vv311"
+	grades = __get_student_grades(student_id)
+	exercises = __get_assigned_exercises(student_id)
+	terms = __get_terms_for_student(student_id)
 	context = Context( {'name': "Varun Verma",
-						'grades' : grades})
+						'grades' : grades, 
+						'exercises' : exercises,
+						'terms': terms})
 	return HttpResponse(template.render(context))	
 
 
+def __get_terms_for_student(student_id):
+	terms = []
+	for i in range(1,5):
+		terms.append(str(i))
+	return terms
 
 def __get_student_grades(student_id):
 	return [["1", "Variable Assignment", "Addition", 40] ,
 			["2", "Conditionals", "If-then-else", 70]
 			]
+
+def __get_assigned_exercises(student_id):
+	return [["Var Assignment", ["subtraction"] ],
+			["Conditionals", ["If-then-else"] ],
+			["For Loop",["Print 10 numbers", "Sum an Array"] ]]
