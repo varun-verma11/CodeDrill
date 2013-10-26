43acd02dab9625e571767a95d906613699bcb1c7
Mihai Popescu <popescu.mihai.mp@gmail.com>
2013-10-26 10:41:41 +0300

    -- small app for authentication, will probably add it soon to the installed apps -- database modified for easier querying -- queries for teacher_view coming next

--- djangoSRV/exercises/models.py
+++ djangoSRV/exercises/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# DID: explicit reference for foreign keys;
+#      many-to-many relationship for course registration
 # TODO: think about separating whether centralising it like this is optimal
+
+# easier to just use this as choices for school years in course 
+
 
 class Exercise(models.Model):
     ex_id = models.AutoField('Exercise ID', primary_key=True)
@@ -11,27 +16,44 @@ class Exercise(models.Model):
 
 
 class ModelSolution(models.Model):
-    ex_id = models.ForeignKey('Exercise', primary_key=True)
+    ex_id = models.ForeignKey('Exercise', to_field='ex_id', primary_key=True)
     content = models.TextField('Content')
 
 
 class StudentSubmission(models.Model):
-    stu_id = models.ForeignKey('Student')
-    ex_id = models.ForeignKey('Exercise')
+    stu_id = models.ForeignKey('Student', to_field='stu_id')
+    ex_id = models.ForeignKey('Exercise', to_field='ex_id')
     content = models.TextField('Content', blank=True)
     submit_time = models.DateTimeField('Time of Submission',
         auto_now_add=True)
 
 
 class Test(models.Model):
-    ex_id = models.ForeignKey('Exercise')
+    ex_id = models.ForeignKey('Exercise', to_field='ex_id')
     test_content = models.TextField('Test Content')
 
 
 class Course(models.Model):
-    c_id = models.AutoField('Course ID' , primary_key=True)
+    # easier to just use this as choices for school years in course 
+    SCHOOL_YEAR_CHOICES = (
+        ( 1 , '1'),
+        ( 2 , '2'),
+        ( 3 , '3'),
+        ( 4 , '4'),
+        ( 5 , '5'),
+        ( 6 , '6'),
+        ( 7 , '7'),
+        ( 8 , '8'),
+        ( 9 , '9'),
+        ( 10 , '10'),
+        ( 11 , '11'),
+        ( 12 , '12'),
+    )
+    c_id = models.AutoField('Course ID', primary_key=True)
     name = models.CharField('Course Name', max_length=50)
-    tch_id = models.ForeignKey('Teacher')
+    year = models.IntegerField('Year', choices = SCHOOL_YEAR_CHOICES)
+    tch_id = models.ForeignKey('Teacher', to_field = 'tch_id')
+    students = models.ManyToManyField('Student')
 
 
 class Teacher(models.Model):
