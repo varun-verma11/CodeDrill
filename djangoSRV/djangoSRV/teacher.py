321eca7c6aca923c81297e2b7eb94e969ff1adee
Mihai Jiplea <mihai@jiplea.com>
2013-10-26 14:05:03 +0100

    QUery changes

--- djangoSRV/djangoSRV/teacher.py
+++ djangoSRV/djangoSRV/teacher.py
@@ -5,15 +5,27 @@ def listAllTeachers(request):
     teachers = Teacher.objects.all()
     ret = []
     for teacher in teachers:
-        print teacher.name
+        #print teacher.name
         ret.append(teacher.name)
     return ret
 
 def viewSubmissions(request):
+    print request.method
     submissions = StudentSubmission.objects.all()
     ret = []
     for submission in submissions:
-        print submission.stu_id, submission.ex_id, submission.content, submission.submit_time
+        print sub
+        submission.submit_timemission.stu_id, submission.ex_id, submission.content, submission.submit_time
+        ret2 = []
+        ret2.append(submission.stu_id)
+        ret2.append(submission.ex_id)
+        ret2.append(submission.content)
+        ret2.append(submission.submit_time)
+        ret.append(ret2)
+    return ret
+
+def authenticateStudent(request):
+    pass
 
 def sendExercisesToClass(request):
     pass
