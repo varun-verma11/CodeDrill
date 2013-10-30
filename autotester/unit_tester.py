c53b9c024f7813f26bdc212aaed3503d2b816b98
Mihai Popescu <popescu.mihai.mp@gmail.com>
2013-10-30 23:35:01 +0200

    --Python Hack for the autotester. Needs further consideration --Small __unicode__ addition

--- autotester/unit_tester.py
+++ autotester/unit_tester.py
@@ -6,7 +6,8 @@ import unittest
 #def autotester(self, student_code, test_code):
 def autotester():
 
-  student_code = '''def from1to5():
+  student_code = '''
+def from1to5():
     ret = []
     for i in range(6):
         ret.append(i)
@@ -21,15 +22,14 @@ class Tester(unittest.TestCase):
         stud_output = from1to5()
         self.assertListEqual(stud_output, [0, 1, 2, 3, 4, 5])
 
-
-unittest.main(module=__name__)
+#unittest.main()
 '''
   test_code = student_code+unit_test
   #exec student_code 
   print test_code
   test_output = StringIO()
   sys.stdout = test_output
-  exec test_code
+  exec test_code in globals() #THIS HACK NEEDS SERIOUS CONSIDERATION
   sys.stdout = sys.__stdout__
   print test_output.getvalue()
   
@@ -39,6 +39,7 @@ unittest.main(module=__name__)
     print ("FAIL")
   else:
     print ("PASS")
+
 
 if __name__ == "__main__":
 	autotester()
