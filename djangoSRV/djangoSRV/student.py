fcdbaa5493cfcefa69924f549db1f7cc7d0fb7d6
Mihai Jiplea <mihai@jiplea.com>
2013-11-04 21:42:10 +0000

    Quries for exercise submission

--- djangoSRV/djangoSRV/student.py
+++ djangoSRV/djangoSRV/student.py
@@ -1,7 +1,12 @@
 from model.models import Student
 
 def submit_exercise(request):
-    pass
+    parameters = REQUEST.GET
+    stu_id = parameters["stu_id"]
+    ex_id  = parameters["ex_id"]
+    score  = parameters["score"]
+    row = LatestStudentScore(stu_id = stu_id, ex_id = ex_id, score = score)
+    row.save()
 
 def get_score(request):
     pass
