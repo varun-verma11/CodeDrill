f0234945cba4fd9cdfd91b19756f0f791c1d79ef
Mihai Popescu <popescu.mihai.mp@gmail.com>
2013-11-06 01:43:05 +0200

    -- changes in the way autotester works. it should be fine for now -- wrote and tested the case of submitting an exercise with wrong/right code. seems to be working -- need to think about what to allow in a test suite

--- djangoSRV/autotester/unit_tester.py
+++ djangoSRV/autotester/unit_tester.py
@@ -1,59 +1,30 @@
 import sys
-# from StringIO import StringIO
 from types import ModuleType
+import traceback
 import unittest
 
 
 # pass student code and unit test file
 # def autotester(self, student_code, test_code):
-def autotester():
+def autotester(student_code, unit_test):
 
-    student_code = '''
-def from1to5():
-    ret = []
-    for i in range(6):
-        ret.append(i)
-
-    return ret
-'''
-    
-    unit_test = '''
-import unittest
-class Tester(unittest.TestCase):
-    def test_1to5(self):
-        stud_output = from1to5()
-        self.assertListEqual(stud_output, [0, 1, 2, 3, 4, 5])
-'''
-
-    #Assumes the testing class to be called tester. for now
-    test_code = '''
-{testModule}.Tester = Tester
-{testModule}.testResult=unittest.main(module={testModule}, exit=False, verbosity=3)
-'''
+    # Changing the format such that you can put your own run code
+    # test_code = '''
+    # {testModule}.Tester = Tester
+    # {testModule}.testResult=unittest.main(module={testModule}, exit=False, verbosity=3)
+    # '''
     mockModule = 'm'
-    test_code = student_code + unit_test + test_code.format(testModule=mockModule)
-    # exec student_code 
-    # print test_code
-    # test_output = StringIO()
-    # sys.stdout = test_output
+    #test_code = student_code + '\n' + unit_test + '\n' + test_code.format(testModule=mockModule)
+    test_code = student_code + '\n' + unit_test.format(testModule=mockModule)
     envDict = {mockModule: ModuleType(mockModule)}
     try:
         exec test_code in envDict  #HACK SOLVED FOR NOW
     except:
-        print "Something wrong happened while executing the test code"
+        print traceback.format_exc()
+        return [0,0,0]
 
-    result = envDict['m'].testResult.result
-    print [len(result.errors), len(result.failures), result.testsRun]
-    # sys.stdout = sys.__stdout__
-    # print test_output.getvalue()
-  
-    # test_code is just the expected output
-    # ifailed = "FAILED"
-    # if (failed in test_output.getvalue()):
-    #     print ("FAIL")
-    # else:
-    #     print ("PASS")
-
+    result = envDict['m'].testResult
+    return [len(result.errors), len(result.failures), result.testsRun]
 
 if __name__ == "__main__":
 	autotester()
