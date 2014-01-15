64bb51c5a646a5c02f17a8c5ad91f261228186d7
Varun Verma <vv311@imperial.ac.uk>
2014-01-15 00:30:54 +0000

    fixed the bug for tick not appearing after auto check

--- djangoSRV/templates/static/js/submit_code.js
+++ djangoSRV/templates/static/js/submit_code.js
@@ -1,6 +1,5 @@
 function submit_code(ex_id) {
   var csrftoken = getCsrfToken();
-  alert("start");
   $.ajax({
         type: 'POST',
         url: '/submit-code/'+ex_id+"/",
@@ -22,7 +21,6 @@ function submit_code(ex_id) {
                   $('#ok_'+ex_id).hide();
                   $('#wrong_'+ex_id).show();
                 }
-                alert("done");
               }
     });
   return false;
